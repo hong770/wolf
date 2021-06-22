51bc6e4afa67f558ac7cd0c1793cd867ad86d331
030030 <andybaby159357@gmail.com>
2021-06-22 12:08:23 +0800

    加入專案檔案。

---
+++ WebApplication4/WebForm1.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+       <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:TextBox ID="TextBox1" runat="server" EnableTheming="False" Height="43px" Width="444px" Enabled="False">0</asp:TextBox>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 輸入中的數字:<asp:Label ID="lable" runat="server" EnableTheming="True"></asp:Label>
+        &nbsp; 上一個數字 :<asp:Label ID="lable0" runat="server" EnableTheming="True">0</asp:Label>
+           確認是否初次輸入:<asp:Label ID="lable1" runat="server" EnableTheming="True">0</asp:Label>
+        &nbsp; 儲存運算:
+            <asp:Label ID="lable2" runat="server" EnableTheming="True">0</asp:Label>
+        &nbsp;是否使用過等於:<asp:Label ID="lable3" runat="server" EnableTheming="True">0</asp:Label>
+        &nbsp;是否使用過運算 :<asp:Label ID="lable4" runat="server" EnableTheming="True">0</asp:Label>
+           連續運算<asp:Label ID="lable5" runat="server" EnableTheming="True">0</asp:Label>
+        <p>
+            <asp:Button ID="Button1" runat="server" Height="47px" OnClick="Button1_Click" Text="1" Width="83px" />
+            <asp:Button ID="Button2" runat="server" Height="47px" OnClick="Button2_Click" Text="2" Width="83px" />
+            <asp:Button ID="Button3" runat="server" Height="47px" OnClick="Button3_Click" Text="3" Width="83px" />
+            <asp:Button ID="Button13" runat="server" Height="47px" OnClick="Button13_Click" Text="*" Width="83px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </p>
+        <p>
+            <asp:Button ID="Button4" runat="server" Height="47px" Text="4" OnClick="Button4_Click" Width="83px" />
+            <asp:Button ID="Button5" runat="server" Height="47px" Text="5" OnClick="Button5_Click"  Width="83px" />
+            <asp:Button ID="Button6" runat="server" Height="47px" Text="6" OnClick="Button6_Click" Width="83px" />
+            <asp:Button ID="Button14" runat="server" Height="47px" Text="/" OnClick="Button14_Click" Width="83px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button16" runat="server" Height="47px" Text="儲存" Width="83px" OnClick="Button16_Click" />
+        </p>
+        <p>
+            <asp:Button ID="Button7" runat="server" Height="47px" Text="7" OnClick="Button7_Click"  Width="83px" />
+            <asp:Button ID="Button8" runat="server" Height="47px" Text="8" OnClick="Button8_Click" Width="83px" />
+            <asp:Button ID="Button9" runat="server" Height="47px" Text="9" OnClick="Button9_Click" Width="83px" />
+            <asp:Button ID="Button15" runat="server" Height="47px" Text="=" OnClick="Button15_Click" Width="83px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button17" runat="server" Height="47px" Text="貼上" Width="83px" OnClick="Button17_Click" />
+        </p>
+        <p>
+            <asp:Button ID="Button10" runat="server" Height="47px" Text="+" OnClick="Button10_Click" Width="83px" />
+            <asp:Button ID="Button11" runat="server" Height="47px" Text="0" OnClick="Button11_Click" Width="83px" />
+            <asp:Button ID="Button12" runat="server" Height="47px" Text="-" OnClick="Button12_Click" Width="83px" />
+            <asp:Button ID="Button18" runat="server" Height="47px" Text="重製" OnClick="Button18_Click" Width="83px" />
+        &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 儲存值:<asp:Label ID="Label2" runat="server"> </asp:Label>
+        </p>
+           <p>
+               使用紀錄: <asp:Label ID="Label3" runat="server"> </asp:Label>
+        </p>
+        <br />
+    </form>
+</body>
+</html>
---
+++ WebApplication4/WebForm1.aspx.cs
@@ -0,0 +1,502 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4
+//Lable1確認是否沒選運算
+//Lable2紀錄數字
+//Lable3記錄過程
+//lable儲存輸入數字
+//lable0儲存上一個輸入數字OR運算結果
+//lable1確認數字重新輸入
+//lable2儲存運算
+//lable3是否使用過等於
+//lable4是否使用過運算
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        public float num;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(lable3.Text) == 1) {
+                Label3.Text = Label3.Text + "<br>";
+                lable0.Text = "0";
+            }
+            if (int.Parse(lable1.Text) == 1 )
+            {
+                lable.Text = "1";
+                lable1.Text = "0";
+            }
+            else
+            {
+                lable.Text = lable.Text + "1";
+            }
+            TextBox1.Text = lable.Text;
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(lable3.Text) == 1)
+            {
+                Label3.Text = Label3.Text + "<br>";
+                lable0.Text = "0";
+            }
+            if (int.Parse(lable1.Text) == 1)
+            {
+                lable.Text = "2";
+                lable1.Text = "0";
+            }
+            else
+            {
+                lable.Text = lable.Text + "2";
+            }
+            TextBox1.Text = lable.Text;
+        }
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(lable3.Text) == 1)
+            {
+                Label3.Text = Label3.Text + "<br>";
+                lable0.Text = "0";
+            }
+            if (int.Parse(lable1.Text) == 1)
+            {
+                lable.Text = "3";
+                lable1.Text = "0";
+            }
+            else
+            {
+                lable.Text = lable.Text + "3";
+            }
+            TextBox1.Text = lable.Text;
+        }
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(lable3.Text) == 1)
+            {
+                Label3.Text = Label3.Text + "<br>";
+                lable0.Text = "0";
+            }
+            if (int.Parse(lable1.Text) == 1)
+            {
+                lable.Text = "4";
+                lable1.Text = "0";
+            }
+            else
+            {
+                lable.Text = lable.Text + "4";
+            }
+            TextBox1.Text = lable.Text;
+        }
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(lable3.Text) == 1)
+            {
+                Label3.Text = Label3.Text + "<br>";
+                lable0.Text = "0";
+            }
+            if (int.Parse(lable1.Text) == 1)
+            {
+                lable.Text = "5";
+                lable1.Text = "0";
+            }
+            else
+            {
+                lable.Text = lable.Text + "5";
+            }
+            TextBox1.Text = lable.Text;
+        }
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(lable3.Text) == 1)
+            {
+                Label3.Text = Label3.Text + "<br>";
+                lable0.Text = "0";
+            }
+            if (int.Parse(lable1.Text) == 1)
+            {
+                lable.Text = "6";
+                lable1.Text = "0";
+            }
+            else
+            {
+                lable.Text = lable.Text + "6";
+            }
+            TextBox1.Text = lable.Text;
+        }
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(lable3.Text) == 1)
+            {
+                Label3.Text = Label3.Text + "<br>";
+                lable0.Text = "0";
+            }
+            if (int.Parse(lable1.Text) == 1)
+            {
+                lable.Text = "7";
+                lable1.Text = "0";
+            }
+            else
+            {
+                lable.Text = lable.Text + "7";
+            }
+            TextBox1.Text = lable.Text;
+        }
+        protected void Button8_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(lable3.Text) == 1)
+            {
+                Label3.Text = Label3.Text + "<br>";
+                lable0.Text = "0";
+            }
+            if (int.Parse(lable1.Text) == 1)
+            {
+                lable.Text = "8";
+                lable1.Text = "0";
+            }
+            else
+            {
+                lable.Text = lable.Text + "8";
+            }
+            TextBox1.Text = lable.Text;
+        }
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(lable3.Text) == 1)
+            {
+                Label3.Text = Label3.Text + "<br>";
+                lable0.Text = "0";
+            }
+            if (int.Parse(lable1.Text) == 1)
+            {
+                lable.Text = "9";
+                lable1.Text = "0";
+            }
+            else
+            {
+                lable.Text = lable.Text + "9";
+            }
+            TextBox1.Text = lable.Text;
+        }
+        protected void Button11_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(lable3.Text) == 1)
+            {
+                Label3.Text = Label3.Text + "<br>";
+                lable0.Text = "0";
+            }
+            if (int.Parse(lable1.Text) == 1)
+            {
+                lable.Text = "0";
+                lable1.Text = "0";
+            }
+            else
+            {
+                lable.Text = lable.Text + "0";
+            }
+            TextBox1.Text = lable.Text;
+        }
+        protected void Button10_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(lable5.Text) == 0)
+            {
+                lable0.Text = lable.Text;
+                lable.Text = ""; 
+                Label3.Text = Label3.Text + lable0.Text + "+";
+                lable5.Text = "1";
+            }
+            else
+            {
+                if (int.Parse(lable2.Text) == 1)
+                {
+                    Label3.Text = Label3.Text + lable.Text + "+";
+                    num = float.Parse(lable0.Text) + float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                }
+                if (int.Parse(lable2.Text) == 2)
+                {
+                    Label3.Text = Label3.Text + lable.Text + "-";
+                    num = float.Parse(lable0.Text) - float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                }
+                if (int.Parse(lable2.Text) == 3)
+                {
+                    Label3.Text = Label3.Text + lable.Text + "+";
+                    num = float.Parse(lable0.Text) * float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                }
+                if (int.Parse(lable2.Text) == 4)
+                {
+                    Label3.Text = Label3.Text + lable.Text + " +";
+                    num = float.Parse(lable0.Text) / float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                }
+            }
+            TextBox1.Text = lable0.Text;
+            
+            lable2.Text = "1";
+            lable1.Text = "1";
+            lable3.Text = "0";
+        }
+
+
+        protected void Button12_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(lable5.Text) == 0)
+            {
+                lable0.Text = TextBox1.Text;
+                lable.Text = "";
+                Label3.Text = Label3.Text + lable0.Text + "-";
+                lable5.Text = "1";
+            }
+            else
+            {
+                if (int.Parse(lable2.Text) == 1)
+                {
+                    Label3.Text = Label3.Text + lable.Text + "-";
+                    num = float.Parse(lable0.Text) + float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                }
+                if (int.Parse(lable2.Text) == 2)
+                {
+                    Label3.Text = Label3.Text + lable.Text + "-";
+                    num = float.Parse(lable0.Text) - float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                }
+                if (int.Parse(lable2.Text) == 3)
+                {
+                    Label3.Text = Label3.Text + lable.Text + "-";
+                    num = float.Parse(lable0.Text) * float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                }
+                if (int.Parse(lable2.Text) == 4)
+                {
+                    Label3.Text = Label3.Text + lable.Text + "-";
+                    num = float.Parse(lable0.Text) / float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                }
+            }
+            TextBox1.Text = lable0.Text;
+
+            lable2.Text = "2";
+            lable1.Text = "1";
+            lable3.Text = "0";
+        }
+        protected void Button13_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(lable5.Text) == 0)
+            {
+                lable0.Text =TextBox1.Text;
+                lable.Text = "";
+                Label3.Text = Label3.Text + lable0.Text + "*";
+                lable5.Text = "1";
+            }
+            else
+            {
+                if (int.Parse(lable2.Text) == 1)
+                {
+                    Label3.Text = Label3.Text + lable.Text + "*";
+                    num = float.Parse(lable0.Text) + float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                }
+                if (int.Parse(lable2.Text) == 2)
+                {
+                    Label3.Text = Label3.Text + lable.Text + "*";
+                    num = float.Parse(lable0.Text) - float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                }
+                if (int.Parse(lable2.Text) == 3)
+                {
+                    Label3.Text = Label3.Text + lable.Text + "*";
+                    num = float.Parse(lable0.Text) * float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                }
+                if (int.Parse(lable2.Text) == 4)
+                {
+                    Label3.Text = Label3.Text + lable.Text + "*";
+                    num = float.Parse(lable0.Text) / float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                }
+            }
+            TextBox1.Text = lable0.Text;
+
+            lable2.Text = "3";
+            lable1.Text = "1";
+            lable3.Text = "0";
+        }
+        protected void Button14_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(lable5.Text) == 0)
+            {
+                lable0.Text = TextBox1.Text;
+                lable.Text = "";
+                Label3.Text = Label3.Text + lable0.Text + " /";
+                lable5.Text = "1";
+            }
+            else
+            {
+                if (int.Parse(lable2.Text) == 1)
+                {
+                    Label3.Text = Label3.Text + lable.Text + "/";
+                    num = float.Parse(lable0.Text) + float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                }
+                if (int.Parse(lable2.Text) == 2)
+                {
+                    Label3.Text = Label3.Text + lable.Text + "/";
+                    num = float.Parse(lable0.Text) - float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                }
+                if (int.Parse(lable2.Text) == 3)
+                {
+                    Label3.Text = Label3.Text + lable.Text + "/";
+                    num = float.Parse(lable0.Text) * float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                }
+                if (int.Parse(lable2.Text) == 4)
+                {
+                    Label3.Text = Label3.Text + lable.Text + " /";
+                    num = float.Parse(lable0.Text) / float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                }
+            }
+            TextBox1.Text = lable0.Text;
+
+            lable2.Text = "4";
+            lable1.Text = "1";
+            lable3.Text = "0";
+        }
+        protected void Button15_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(lable2.Text) == 1)
+            {
+                if (int.Parse(lable3.Text) == 0)
+                {
+                    Label3.Text = Label3.Text + lable.Text + "=";
+                    num = float.Parse(lable.Text) + float.Parse(lable0.Text);
+                    lable0.Text = num.ToString();
+                    Label3.Text = Label3.Text + lable0.Text;
+                    TextBox1.Text = lable0.Text;
+                    lable1.Text = "1";
+                    lable3.Text = "1";
+                }
+                else {
+                    Label3.Text = Label3.Text + "+" + lable.Text + "=";
+                    num = float.Parse(lable.Text) + float.Parse(lable0.Text);
+                    lable0.Text = num.ToString();
+                    Label3.Text = Label3.Text + lable0.Text + "      ";
+                    TextBox1.Text = lable0.Text;
+                    lable1.Text = "1";
+                }
+            }
+            if (int.Parse(lable2.Text) == 2)
+            {
+                if (int.Parse(lable3.Text) == 0)
+                {
+                    Label3.Text = Label3.Text + lable.Text + "=";
+                    num = float.Parse(lable0.Text) - float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                    Label3.Text = Label3.Text + lable0.Text;
+                    TextBox1.Text = lable0.Text;
+                    lable1.Text = "1";
+                    lable3.Text = "1";
+                }
+                else
+                {
+                    Label3.Text = Label3.Text + "-" + lable.Text + "="; 
+                    num = float.Parse(lable0.Text) - float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                    Label3.Text = Label3.Text + lable0.Text + "      ";
+                    TextBox1.Text = lable0.Text;
+                    lable1.Text = "1";
+                }
+            }
+            if (int.Parse(lable2.Text) == 3)
+            {
+                if (int.Parse(lable3.Text) == 0)
+                {
+                    Label3.Text = Label3.Text + lable.Text + "=";
+                    num = float.Parse(lable0.Text) * float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                    Label3.Text = Label3.Text + lable0.Text;
+                    TextBox1.Text = lable0.Text;
+                    lable1.Text = "1";
+                    lable3.Text = "1";
+                }
+                else
+                {
+                    Label3.Text = Label3.Text + "*" + lable.Text + "="; 
+                    num = float.Parse(lable0.Text) * float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                    Label3.Text = Label3.Text + lable0.Text + "      ";
+                    TextBox1.Text = lable0.Text;
+                    lable1.Text = "1";
+                }
+            }
+            if (int.Parse(lable2.Text) == 4)
+            {
+                if (int.Parse(lable3.Text) == 0)
+                {
+                    Label3.Text = Label3.Text + lable.Text + "=";
+                    num =  float.Parse(lable0.Text)/float.Parse(lable.Text) ;
+                    lable0.Text = num.ToString();
+                    Label3.Text = Label3.Text + lable0.Text;
+                    TextBox1.Text = lable0.Text;
+                    lable1.Text = "1";
+                    lable3.Text = "1";
+                }
+                else
+                {
+                    Label3.Text = Label3.Text + "/" + lable.Text + "="; 
+                    num = float.Parse(lable0.Text) / float.Parse(lable.Text);
+                    lable0.Text = num.ToString();
+                    Label3.Text = Label3.Text + lable0.Text+"      ";
+                    TextBox1.Text = lable0.Text;
+                    lable1.Text = "1";
+                }
+            }
+            lable5.Text = "0";
+
+        }
+        protected void Button18_Click(object sender, EventArgs e)
+        {
+            lable.Text = "";
+            lable0.Text = "0";
+            lable1.Text = "0";
+            lable2.Text = "0";
+            lable3.Text = "0";
+            lable4.Text = "0";
+            Label2.Text = "";
+            Label3.Text = "";
+            TextBox1.Text = "";
+        }
+
+        protected void Button16_Click(object sender, EventArgs e)
+        {
+            Label2.Text = TextBox1.Text;
+        }
+
+        protected void Button17_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = Label2.Text;
+            lable.Text = Label2.Text;
+            if (int.Parse(lable3.Text) == 1)
+            {
+                Label3.Text = Label3.Text + "<br>";
+                lable0.Text = "0";
+            }
+            lable3.Text = "0";
+
+        }
+    }
+}
+
+
